a6835e2b5810df6e1ebd75b3dfd0d2c2d1fe428b
Vukašin Stanković <vule.stankovic2@gmail.com>
2024-05-22 13:56:27 +0200

    Basic Express project setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World");
+import cors from "cors";
+import morgan from "morgan";
+import express from "express";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+
+const port = 4001;
+app.listen(port, () => {
+    console.log("App started and listening on port: " + port);
+});
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World From ExpressJS and TypeScript!",
+    });
+});
